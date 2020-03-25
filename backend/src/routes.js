f5ddc83aa5fe9763cd8e3931aab8343d347538d1
Roberth Silva <roberthvipleiloes@hotmail.com>
2020-03-24 21:18:12 -0300

    Back-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const ongController = require('./controllers/OngController');
+const incidentController = require('./controllers/IncidentController');
+const profileController = require('./controllers/ProfileController');
+const sessionController = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.post('/sessions', sessionController.create);
+
+routes.get('/ongs', ongController.index);
+routes.post('/ongs', ongController.create);
+
+routes.get('/profile', profileController.index);
+
+routes.get('/incidents', incidentController.index);
+routes.post('/incidents', incidentController.create);
+routes.delete('/incidents/:id', incidentController.delete);
+
+module.exports = routes;
+
+
+/* routes.post('/users', (request, response) => {
+    const params = request.body;
+    console.log(params);
+    
+    return response.json({
+        evento: 'Semana OmniStack 11.0',
+        aluno: 'Ramiro Neto'
+    });
+}); */
